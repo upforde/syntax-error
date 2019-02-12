ee67b9cd27b3dcbe3af3faecd0ddd862e9ef906f
Dank <upforde1@gmail.com>
2019-02-12 18:26:53 +0100

    JSON implementation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import React from 'react';
 import Grid from './Components/Grid';
 import Header from './Components/Header';
 import FlipButton from './Components/FlipButton';
-import imgURLS from './imgURLS';
+import imgURLS from './Components/imgURLS.json';
 import './index.css';
 
 class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            buttonData: Array.from(imgURLS),
+            isFlippable: true
+        };
+        this.changeFlippable = this.changeFlippable.bind(this);
+    }
+
     assignButtons(){
-        return Array.from(imgURLS).map(imgData => <FlipButton key={imgData.key} imgURL={imgData.imgURL} />);
+        return this.state.buttonData.map(imgData =>
+            <FlipButton
+                key={ imgData.key } 
+                buttonID={ imgData.key } 
+                imgURL={ imgData.imgURL }
+                changeFlippable={ this.changeFlippable }
+                isFlippable={ this.state.isFlippable }
+            />);
+    }
+
+    changeFlippable(e) {
+        this.setState(prevState => ({ isFlippable: !prevState.isFlippable }));
     }
 
     render(){
--- src/Components/FlipButton.js
+++ src/Components/FlipButton.js
@@ -1,26 +1,62 @@
 import React, { Component } from 'react';
 import ReactCardFlip from 'react-card-flip';
 import Button from 'react-bootstrap/Button';
+import Data from '../data.json';
 
 class FlipButton extends Component {
   constructor() {
     super();
     this.state = {
-      isFlipped: false
+      isFlipped: false,
+      data: {}
     };
-    this.handleClick = this.handleClick.bind(this);
-    this.flipAndCalculate = this.flipAndCalculate.bind(this);
   }
 
-  handleClick(e) {
+  handleClick = (e) => {
     e.preventDefault();
     this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
+    this.props.changeFlippable();
   }
   
-  flipAndCalculate(e) {
+  flipAndCalculate = (e) => {
     e.preventDefault();
+    console.log(this.state.data);
+    //calculate();
     this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
-    console.log("Calculating...");
+    this.props.changeFlippable();
+  }
+
+  calculate(){
+    switch(this.props.buttonID){
+      case "quiz":
+        break;
+      case "screenshot":
+        break;
+      case "audience":
+        break;
+      case "multiple":
+        break;
+      case "music":
+        break;
+      case "crew":
+       break;
+      case "silhouette":
+        break;
+      case "sologame":
+        break;    
+      default:
+        break;  
+    }
+  }
+
+  componentDidMount = () => {
+    for(let i in Data){
+      if(this.props.buttonID === Data[i].key){
+        this.setState({
+          data: Data[i].data
+        });
+      }
+    }
   }
  
   render() {
@@ -28,12 +64,17 @@ class FlipButton extends Component {
       <nav className="button">
         <ReactCardFlip isFlipped={this.state.isFlipped}>
           <div key="front">
-            <img src={this.props.imgURL} onClick={this.flipAndCalculate} className="Button-logo" alt="logo" />
+            <img 
+              src={this.props.imgURL} 
+              onClick={ this.props.isFlippable?this.flipAndCalculate:null } 
+              className="Button-logo" 
+              alt="logo" 
+            />
           </div>
           <div key="back">
             <div className="areYouSureAboutThat">
               <Button variant="success" className="areYouSureAboutThatObject">Yes</Button>
-              <Button variant="danger" className="areYouSureAboutThatObject" onClick={this.handleClick}>No</Button>
+              <Button variant="danger" className="areYouSureAboutThatObject" onClick={ this.handleClick }>No</Button>
             </div>
           </div>
         </ReactCardFlip>
